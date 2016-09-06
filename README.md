2f1f299350d5ab5407682fe1c0f2f18ee33c28f5
zacscodingclub <zbaston@gmail.com>
2016-09-06 09:58:58 -0400

    inital commit. basic angular-seed app up and running

---
+++ README.md
@@ -0,0 +1,3 @@
+# Spacebook
+
+An Angular web application built on [https://github.com/angular/angular-seed](angular-seed) and [https://github.com/AlmogBaku/ngFacebook](ngFacebook).  I'm using this as an opportunity to learn the Facebook API within the context of a front end framework, in this case it's AngularJS.
---
+++ app/app.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// Declare app level module which depends on views, and components
+angular.module('myApp', [
+  'ngRoute'
+]).
+config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+  $locationProvider.hashPrefix('!');
+
+  $routeProvider.otherwise({redirectTo: '/view1'});
+}]);
