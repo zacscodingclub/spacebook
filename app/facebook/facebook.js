ff8a3a328d63425ca4a17f2b5f1261075d5630b2
zacscodingclub <zbaston@gmail.com>
2016-09-06 14:34:48 -0400

    setup ngFacebook, load Facebook SDK with no errors

--- app/facebook/facebook.js
+++ app/facebook/facebook.js
@@ -1,13 +1,32 @@
 'use strict';
 
-angular.module('myApp.facebook', ['ngRoute'])
+angular.module('myApp.facebook', ['ngRoute', 'ngFacebook'])
 
-  .config([$routeProvider, function($routeProvider) {
+  .config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/facebook', {
-      templateUrl: 'facebook/facebook.html'
+      templateUrl: 'facebook/facebook.html',
       controller: 'FacebookController'
     });
   }])
+
+  .config(function($facebookProvider) {
+    $facebookProvider.setAppId('1502992906393073');
+    $facebookProvider.setPermissions("email",
+                                     "public_profile",
+                                     "user_posts",
+                                     "publish_actions",
+                                     "user_photos");
+  })
+
+  .run(function($rootScope) {
+    (function(d, s, id){
+       var js, fjs = d.getElementsByTagName(s)[0];
+       if (d.getElementById(id)) {return;}
+       js = d.createElement(s); js.id = id;
+       js.src = "//connect.facebook.net/en_US/sdk.js";
+       fjs.parentNode.insertBefore(js, fjs);
+     }(document, 'script', 'facebook-jssdk'));
+  })
 
   .controller('FacebookController', [function() {
 
